5317e24ffe7d5d73c90122b9c248aa2bf3102549
AkkuTUD <77341846+AkkuTUD@users.noreply.github.com>
2022-10-18 15:27:01 +0200

    Update Project-2.R

--- Project-2.R
+++ Project-2.R
@@ -10,27 +10,23 @@ head(data)
 
 summary(data$price)
 
-
 group_by(data, model) %>%
   summarise(count = n(),mean = mean(price, na.rm = TRUE),median = median(price,na.rm = TRUE),sd = sd(price, na.rm = TRUE), var = var(price, na.rm=TRUE)) 
 
 #Data processing
-#1
+#1 Convert car price to log price
 logprice <- log(data$price)
 head(data)
-#2
+#2Convert mpg to litre per 100
 lp100 <- 282.48/(data$mpg)
 head(data)
-#3
+#Add column car age 
 cars_age <- 2020-(data$year)
 head(data)
 
 data = cbind(data,logprice,lp100,cars_age)
 
 head(data)
-
-#, data$mileage, data$logprice, data$lp100, data$cars_age
-
 
 #Estimate model
 
@@ -63,12 +59,12 @@ lmod = lm(logprice~model+cars_age+mileage+lp100+fuelType+engineSize+tax+
             transmission,data = data)
 
 
-###Best Subset Selection for all the subsets of the predicates
+#Best Subset Selection for all the subsets of the predicates
 best_data<-ols_step_all_possible(lmod)
 head(best_data)
 
 nrow(best_data)
-##255
+#255
 
 ## Tasks 2 compare using AIC and BIC as model selection criteria
 
@@ -89,11 +85,11 @@ BIC
 
 variables_in_aic<-best_data[AIC_index,3]
 variables_in_aic
-"model cars_age mileage lp100 fuelType engineSize transmission"
+#"model cars_age mileage lp100 fuelType engineSize transmission"
 
 variables_in_bic<-best_data[BIC_index,3]
 variables_in_bic
-"model cars_age mileage fuelType engineSize transmission"
+#"model cars_age mileage fuelType engineSize transmission"
 
 lm_bic = lm(logprice ~ model+cars_age+mileage+fuelType+engineSize+transmission , data = data)
 
